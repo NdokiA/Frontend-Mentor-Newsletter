4e8764b0986ec9e4e905a3027154061608889f58
Ndiko-A <andikoputrapk@student.ub.ac.id>
2025-01-20 22:41:22 +0700

    Fix JS

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
 const emailInput = document.querySelector('.email-input')
 const errorMsg = document.querySelector('.error-msg')
 
+emailInput.addEventListener('focus', () => {
+    errorMsg.style.display = 'none';
+});
+
 emailInput.addEventListener('blur', () => {
     if (emailInput.validity.valid) {
         errorMsg.style.display = 'none'
